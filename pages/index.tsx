937ab0784c3b5faf482f5b4cd708678064ab87f0
Aakash Pandya <apandya@mobiquityinc.com>
2022-12-23 22:43:52 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,5 @@
-import { Inter } from "@next/font/google";
-import Switch from "@mui/material/Switch";
-import styles from "../styles/Home.module.css";
-
-const label = { inputProps: { "aria-label": "Switch demo" } };
+import Dashboard from "./dashboard";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <div>
-        <span>With default Theme:</span>
-      </div>
-      <Switch {...label} defaultChecked />
-      <Switch {...label} />
-      <Switch {...label} disabled defaultChecked />
-    </div>
-  );
+  return <Dashboard />;
 }
