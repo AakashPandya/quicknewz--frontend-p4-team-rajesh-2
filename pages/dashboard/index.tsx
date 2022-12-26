8e6fd40f408ec36e3df5368f190e61dc4c8b037f
Aakash Pandya <apandya@mobiquityinc.com>
2022-12-26 22:42:52 +0530

    fixes

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -10,15 +10,15 @@ export default function Dashboard() {
   const [headlines, setHeadlines] = useState([]);
   useEffect(() => {
     console.log("I AM HERE");
-    // try {
-    //   fetch(
-    //     "https://us-central1-storied-groove-370117.cloudfunctions.net/app/headlines"
-    //   )
-    //     .then((response) => response.json())
-    //     .then((data) => setHeadlines(data));
-    // } catch (ex) {
-    //   console.log(ex);
-    // }
+    try {
+      fetch(
+        "https://us-central1-storied-groove-370117.cloudfunctions.net/app/headlines"
+      )
+        .then((response) => response.json())
+        .then((data) => setHeadlines(data));
+    } catch (ex) {
+      console.log(ex);
+    }
   }, []);
 
   console.log(headlines);
@@ -26,6 +26,7 @@ export default function Dashboard() {
   return (
     <>
       <Navbar />
+      {headlines}
       <Grid container spacing={3} sx={{ padding: "50px" }}>
         {headlines.map(
           ({ createdAt, provider, logo, imageLink, title }, index) => (
